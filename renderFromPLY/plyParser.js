a0bb77f9e8ce4ca283602b5c711a33d60d4efdc5
lericson <lericson@iastate.edu>
2022-11-29 01:13:46 +0000

    Update renderFromPLY/plyParser.js

--- renderFromPLY/plyParser.js
+++ renderFromPLY/plyParser.js
@@ -1,4 +1,4 @@
-//This will interpret PLY files, starting with binary
+//This will interpret PLY files and convert into Float32Arrays
 
 /** var type        size (bytes)
  * ------------------------
@@ -22,9 +22,12 @@ jQuery.get(plyFile, function(data) {
 
 function readFile() {
     //Read header
-    var elementVal, newline, line;
+    var curVal, newline, line;
     var hasNormals = false;
     plyData = String(plyData);
+    var format;
+
+    var numNormals, numFaces;
 
     while(plyData.length) {
         newline = plyData.indexOf("\n")+1;
@@ -32,15 +35,21 @@ function readFile() {
         plyData = plyData.substring(newline);
         //console.log(plyData);
         
-        elementVal = plyData.match(/element (\w+) (\d+)/); //find first element line
-        if(elementVal) {
-            if(elementVal[1] == "vertex") {
-                var numVerts = parseInt(elementVal[2]);
-                console.log(numVerts);
-                break;
+        curVal = plyData.match(/element (\w+) (\d+)/); //find first element line
+        if(curVal) {
+            if(curVal[0] == "format") {
+                format = curVal[1];
             }
-        } else {
-            //console.log("element line not found");
+
+            if(curVal[1] == "vertex") {
+                numVerts = parseInt(curVal[2]);
+                console.log("Verticies: " + numVerts);
+            }
+
+            if(curVal[1] == "face") {
+                numFaces = parseInt(curVal[2]);
+                console.log("Faces: " + numFaces);
+            }
         }
 
         if(line == "property float nx") hasNormals = true;
@@ -48,6 +57,33 @@ function readFile() {
             console.log("End of header reached");
             break;
         }
+    }
+
+    //Read points
+    var verticies = new Float32Array(numVerts);
+
+    //Reads points in ply ascii format
+    if(format == "ascii") {
+        for(let i = 0; i < numVerts; i++) {
+            newline = plyData.indexOf("\n")+1;
+            line = plyData.substring(0, newline - 1).trim();
+            plyData = plyData.substring(newline);
+
+            curVal = line.split(" ");
+            
+            let x = i * 3;
+            let y = i * 3 + 1;
+            let z = i * 3 + 2;
+
+            verticies[x] = parseFloat(curVal[0]);
+            verticies[y] = parseFloat(curVal[1]);
+            verticies[z] = parseFloat(curVal[2]);
+
+        }
+    }
+
+    if(format == "binary_little_endian") {
+        //not done yet
     }
 }
 
